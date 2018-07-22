aa559e808f2c7c7ceacfc3b484e6ca63625da0b5
Selden Lin <selden.lin@ibm.com@Seldens-MacBook-Pro.local>
2018-07-22 17:36:47 -0400

    Worked on route

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 router.get("/", function(req, res) {
     res.render("home", {title: "home"}) 
+});
+
+router.get("/seasoning/:item", function(req, res) {
+    res.render("seasoning", {title: "item", name: req.params.item});
 })
 
 module.exports = router;
